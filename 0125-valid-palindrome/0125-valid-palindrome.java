a078e71b4a56f77c3f5cf87e08a6add1bd0167a0
SATENDRA KUMAR YADAV <princeworks990@gmail.com>
2026-07-15 16:12:26 +0530

    Time: 14 ms (31.43%), Space: 46.2 MB (31.68%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        int j=s.length()-1;
+        for(int i=0;i<s.length()/2;i++){
+            if(s.charAt(i)!= s.charAt(j)){
+                return false;
+            }
+            j--;
+        }
+        return true;
+    }
+}
