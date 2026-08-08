bd75ca7b23cace34e60e318d74d53c09a001220a
Satendra <princeworks990@gmail.com>
2026-08-08 17:05:15 +0530

    Buuble SOrt ALgorithm

---
+++ Sorting-Algorithms/BubbleSort.java
@@ -0,0 +1,19 @@
+public class BubbleSort{
+    public static void main(String[] args) {
+        int arr[]={1,4,7,10,2,3};
+        int temp=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        //Print
+        for(int a : arr){
+            System.out.print(a + " ");
+        }
+    }
+}
